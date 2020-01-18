4cebdae56e6acd35ff9ccf5a84c44fd50d441201
Brent Jeffson Florendo <brentjeffson@gmail.com>
2020-01-18 20:27:11 +0800

    add function to add and remove from library

--- reader/pages/infopage.py
+++ reader/pages/infopage.py
@@ -1,28 +1,28 @@
 from webscrape2.constants import Source
 from webscrape2.scraper import MangaScraper
-from utils import Library, LIBRARY_PATH
+from utils import Library, LIBRARY_PATH, ACTIVE_CHAPTER
 from functools import partial
 from kivy.core.image import Image as CoreImage
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
 from kivy.properties import Clock
 import requests
 import io
 
 class InfoPage(BoxLayout):
+    archived = BooleanProperty(False)
     chapter_list = ObjectProperty()
     manga_image_view = ObjectProperty()
     manga_title = StringProperty()
 
     nav_library_btn = ObjectProperty()
     manga = None
-    active_chapter = ""
-
     def __init__(self, app, **kwargs):
         super().__init__(**kwargs)
         self.app = app
         
     def get_manga(self, url, source, *_):
+        print(f"Opening -> {url} from {source}")
         self._clean_info()
         resp = requests.get(url)
         if not resp.ok:
@@ -37,20 +37,43 @@ class InfoPage(BoxLayout):
 
         Clock.schedule_once(partial(self._update_info, manga, image_byte, scraper), 0)
 
-    def _add_to_library(self):
-        Library(manga.url, manga.title, self.active_chapter, manga.image_url).save(LIBRARY_PATH)
+    def update_active_chapter(self, chapter_uid):
+        """updates the mangas current `active_chapter` in the `library`"""
+
+        library = Library.load(LIBRARY_PATH)
+        if self.manga.url in library:
+            print(f"Manga -> {self.manga.title} active chapter updated.")
+            info = library[self.manga.url]
+            if float(chapter_uid) > float(info[ACTIVE_CHAPTER]):
+                Library(self.manga.url, self.manga.title, chapter_uid, self.manga.image_url).save(LIBRARY_PATH)
 
     def _clean_info(self):
         self.manga_title = "Loading..."
         self.manga_image_view.texture = None
         self.chapter_list.data = []
 
+    def __remove_from_library(self):
+        pass
+
+    def _add_to_library(self):
+        Library(self.manga.url, self.manga.title, "", self.manga.image_url).save(LIBRARY_PATH)
+
     def _update_info(self, manga, image_byte, scraper, *_):
         self.manga_title = manga.title
+        self.manga = manga
+
+        library = Library.load(LIBRARY_PATH)
+        for url in library:
+            if self.manga.url == url:
+                self.archived = True
+                break
+            else: 
+                self.archived = False
+
         ext = manga.image_url.split(".")[len(manga.image_url.split("."))-1]
         texture = CoreImage(io.BytesIO(image_byte), ext=ext).texture
         self.manga_image_view.texture = texture
         self.manga_image_view.size = texture.size
-        self.manga = manga
+        
         for chapter in manga.chapters:
             self.chapter_list.data.append({"app": self.app, "text": f"({chapter.uid}){chapter.title}", "chapter": chapter, "scraper": scraper})
